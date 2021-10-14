9959efd8b8a67690c8e96a1f33c964c9d20d6b9d
“tayboy217” <tayboy217@icloud.com>
2021-10-14 17:45:58 +0000

    Add All

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c2ed95a3b4590de7cfc4324627c49ccc523394633714c3f58ec0647e85b05fe636a5dd34e02924aad26aa85e289e909487fa6549be3737d0e066ebc55c1fa6f6'
